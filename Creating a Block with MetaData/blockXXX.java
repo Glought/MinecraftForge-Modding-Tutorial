3d4619556ffc1f7c66d55784608c71ac2c988381
helfull <helfull@gmx.de>
2012-07-04 03:20:12 +0200

    Added Creating a Block with MetaData
    MCP 6.2
    Forge 3.3.8.xxx

---
+++ Creating a Block with MetaData/blockXXX.java
@@ -0,0 +1,54 @@
+package net.minecraft.src.tutorial;
+
+import net.minecraft.src.Block;
+import net.minecraft.src.EntityLiving;
+import net.minecraft.src.Material; // Material class to give the block a material
+import net.minecraft.src.StepSound;
+import net.minecraft.src.World;
+import net.minecraft.src.forge.ITextureProvider;
+
+public class blockXXX extends Block implements ITextureProvider {
+
+	protected blockXXX(int blockID, int textureID) {
+		// setting the blockID and the default textureID
+		// giving this block the Material.wood
+		super(blockID, textureID, Material.wood);
+		setHardness(3.1F); //sets the hardness, represents the amount of hits it needs to break
+		setResistance(0.3F); //sets the resistance of the block
+		setLightValue(10); //sets the lighvalue of the block so it acts like glowstone
+		setStepSound(soundWoodFootstep); //sets the sound which is played when a entity runs over it
+		//setBlockUnbreakable() //if you want that the block is unbreakable call this functions
+	}
+
+	@Override
+    protected int damageDropped(int metadata)
+    {
+        return metadata;
+    }
+	
+	//with this function we can set diffrent textures on the sides and metadata
+	//if we dont use this it will use for all side the textureID given in at the
+	//constructor
+	@Override
+	public int getBlockTextureFromSideAndMetadata(int side, int metadata) {
+		if(side == 0 || side == 1) //if side = top or side = bottom return 01 (Hexcode)
+		{
+			if(metadata == 1){ // of metadata = 1 return 04 (hexcode)
+				return 04;
+			}
+			return 01;
+		}
+		//for all othersides return 00
+		//or when metadata == 1 03
+		if(metadata == 1){
+			return 03;
+		}
+		return 00;
+		
+	}
+	
+	@Override
+	public String getTextureFile(){
+		return "/tutorialtextures/blocks.png"; //return the block texture where the block texture is saved in
+	}
+}
---
+++ Creating a Block with MetaData/mod_XXX.java
@@ -0,0 +1,74 @@
+package net.minecraft.src.tutorial;
+
+import net.minecraft.src.BaseMod;
+import net.minecraft.src.Block;
+import net.minecraft.src.Item;
+import net.minecraft.src.ItemStack;
+import net.minecraft.src.ModLoader;
+import net.minecraft.src.forge.MinecraftForgeClient;
+
+//each mod needs to have a class with the name mod_ infront of the name
+public class mod_XXX extends BaseMod {
+	
+	public static Block blockXXX; // Our block we want to create with a recipe
+	public static Item itemXXX; // our item we want to create with a recipe
+	
+	
+	@Override
+	public String getVersion() {
+		return "0.0.1"; // returns the version number as String
+	}
+
+	@Override
+	public void load() {
+		MinecraftForgeClient.preloadTexture("/tutorialtextures/blocks.png");
+		MinecraftForgeClient.preloadTexture("/tutorialtextures/items.png");
+		this.registerBlocks(); //register all blocks
+		this.addNames(); //adding all names
+		this.registerRecipes(); //register all recipes
+	}
+	
+	//private method for proper source
+	private void registerBlocks() {
+		//ModLoader needs to know that you want to add a block to the game
+		// to register a metadata block we need to call this function with the itemblock class as 2nd parameter
+		ModLoader.registerBlock(blockXXX, net.minecraft.src.tutorial.itemblockXXX.class);
+	}
+	
+	//private method for proper source
+	private void addNames(){
+		//this have to be called so modloader knows the itemname and can add it to minecraft
+		ModLoader.addLocalization("blockXXX.name", "Block Log 1");
+		ModLoader.addLocalization("blockXXX2.name", "Block Log 2");
+	}
+	
+	private void registerRecipes(){
+		
+		// Adding a Shaped recipe for our item
+		// we get 2 itemXXX from this recipe
+		ModLoader.addRecipe(new ItemStack(itemXXX, 2), new Object[] 
+		{
+			//the first 3 strings are your recipe in the craftingtable
+			//the char 'X' is set to be a Item.Stick
+			//and the char '+' ist set to be Block.Cobblestone
+			//so our item will me maked out of 6 cobblestone and 1 stick
+			"X X", "X+X", "X X", 'X', Item.stick, '+', Block.cobblestone
+		});
+		
+		// Now we adding a Shapelessrecipe for our Block
+		// we get 16 blockXXX from this recipe while we just need 3 itemXXX in any shape we put it in the crafting
+		// first with metadata = 0, default if no given
+		ModLoader.addShapelessRecipe(new ItemStack(blockXXX, 16), new Object[] {itemXXX, itemXXX, itemXXX});
+		// next one with metadata = 1, the 2nd int parameter in new ItemStack
+		ModLoader.addShapelessRecipe(new ItemStack(blockXXX, 16, 1), new Object[] {itemXXX, itemXXX});
+	}
+	
+	static {
+		Props.initProps("XXX"); //Init the Config with the Name XXX
+		
+		//new blockXXX  | Getting the blockID from the config 
+		//setBlockName  | sets the blockName will be overwritten by modloader
+		blockXXX = new blockXXX(Props.bXXXID, 0).setBlockName("Block XXX");
+		itemXXX = new itemXXX(Props.iXXXID).setIconCoord(0, 0);
+	}
+}
